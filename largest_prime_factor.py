21abed4dcfdc20f510a99b070bb8ac96bc8f1957
Carlator <clorenzozuniga@gmail.com>
2023-09-25 20:25:32 +0200

    - problem 3

---
+++ largest_prime_factor.py
@@ -0,0 +1,33 @@
+"""
+Problem 3:
+What is the largest prime factor of the number 600851475143?
+"""
+from typing import List
+import math
+
+def prime_factors(n: int) -> List[int]:
+    """
+    Find the prime factors a number n
+
+    Args:
+        n (int): Number for which to find prime factors
+
+    Returns:
+        List[int]: List containing all the prime factors
+    """
+    
+    
+    factors = []
+    f = 2 #current factor
+    while f < math.sqrt(n):
+        while n % f == 0:
+            n = n/f
+            factors.append(f)
+        f += 1
+    factors.append(n)
+    return max(factors)
+
+
+print(prime_factors(600851475143)) # Largest prime factor => max in list
+
+
--- multiples3_5.py
+++ multiples3_5.py
@@ -1,5 +1,5 @@
 """
-Challenge 1: 
+Problem 1:
 Find the sum of all the multiples of 3 or 5 below 1000 .
 """
 
