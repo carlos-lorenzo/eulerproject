7e6882b842b47a0eb47e46873dc9b16a9e414762
Carlos <clorenzozuniga@gmail.com>
2023-09-25 16:00:52 +0200

    - First two challenge solved

---
+++ fibonacci_even.py
@@ -0,0 +1,48 @@
+"""
+Challenge 2:
+
+By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+find the sum of the even-valued terms.
+"""
+
+import math
+
+
+def fibonacci(n: int) -> int:
+    """
+    nth term for fibonacci sequence
+    
+    source: https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+    Args:
+        n (int): position of item in sequence
+
+    Returns:
+        int: Number in sequence of position n
+    """
+    
+    
+    PHI = (1 + math.sqrt(5)) / 2
+    PSI = 1 - PHI
+    
+    return round((PHI**n - PSI**n) / ((2 * PHI) - 1))
+
+
+
+sum_terms = 0
+n = 1
+while True:
+    term = fibonacci(n=n)
+    
+    if term > 4000000:
+        break
+    
+    if term % 2 == 0:
+        sum_terms += term
+        
+    n += 1
+    
+print(sum_terms)
+
+
+
---
+++ multiples3_5.py
@@ -0,0 +1,10 @@
+"""
+Challenge 1: 
+Find the sum of all the multiples of 3 or 5 below 1000 .
+"""
+
+multiples = [num for num in range(1000) if num % 3 == 0 or num % 5 == 0]
+
+sum_multiples = sum(multiples)
+
+print(sum_multiples)
